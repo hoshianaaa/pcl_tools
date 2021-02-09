8d1d908f0e1d49615eabedf495b38573cdb6cb63
Ryotaro Hoshina <hoshina12345@gmail.com>
2021-02-09 22:00:37 +0900

    まずまずうまく行った!

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -12,13 +12,13 @@ def matplotlib_plt(X,X2,X3):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
 
-    ax.scatter(X[:,0], X[:,1], X[:,2], c=[0,0,0])
+    #ax.scatter(X[:,0], X[:,1], X[:,2], c=[0,0,0])
     ax.scatter(X2[:,0], X2[:,1], X2[:,2], c=[0.1,1,1])
     ax.scatter(X3[:,0], X3[:,1], X3[:,2], c=[1,1,0.1])
 
     plt.show()
 
-data = pd.read_csv('data2.csv',header=0)
+data = pd.read_csv('data.csv',header=0)
 
 c = data.iloc[:,0:3].values
 d = data.iloc[:,3:6].values
@@ -42,10 +42,9 @@ for i in c:
     a = np.dot(R,i) + t
     result.append(a)
 
-print(np.array(result))
-#matplotlib_plt(c,d,np.array(result))
+#print(np.array(result))
+matplotlib_plt(c,d,np.array(result))
 
-test = [0.05,-0.025,0]
 error_sum = 0
 for i in range(len(d)):
   a = (np.dot(R,c[i]) + t)
@@ -55,13 +54,17 @@ for i in range(len(d)):
   error_sum = error_sum + e
 
 error_mean =  error_sum / len(d)
-print(error_mean)
+print("ERROR:",error_mean)
 
 rot = Rotation.from_dcm(R)
 quat = rot.as_quat()
+euler = rot.as_euler('XYZ')
 print(quat)
 print(t)
 frame1 = "base_link"
 frame2 = "camera_depth_optical_frame"
 print("rosrun tf static_transform_publisher x y z qx qy qz qw frame_id child_frame_id period_in_ms")
+
 print("rosrun tf static_transform_publisher " + str(t[0]) + " " + str(t[1]) + " " + str(t[2]) + " " + str(quat[0]) + " " + str(quat[1]) + " " + str(quat[2]) + " " + str(quat[3]) + " " + frame1 + " " + frame2 + " 100") 
+
+print("rosrun tf static_transform_publisher " + str(t[0]) + " " + str(t[1]) + " " + str(t[2]) + " " + str(euler[0]) + " " + str(euler[1]) + " " + str(euler[2]) + " " + frame1 + " " + frame2 + " 100") 
