d861c3e97f691bd76cded851abd78f1d4d9cd2c7
Ryotaro Hoshina <hoshina12345@gmail.com>
2021-03-31 21:13:33 +0900

    fix camera tf bag

--- src/camera_tf.cpp
+++ src/camera_tf.cpp
@@ -14,62 +14,32 @@
 #include <pcl/common/geometry.h>
 #include <pcl/registration/transformation_estimation_svd.h>
 
+
+
 using namespace std;
 
+double m11, m12, m13, m14;
+double m21, m22, m23, m24;
+double m31, m32, m33, m34;
+
+
 std::string input_cloud_topic_ = "/camera/depth/color/points";
-std::string output_cloud_topic_ = "tf_depth_cloud";
+std::string output_cloud_topic_ = "tf_cloud";
 std::string robot_frame_ = "base_link";
 
 class CameraTF
 {
 public:
-  CameraTF(const std::string& fname)
+  CameraTF()
   {
-
     output_cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>(output_cloud_topic_,1, false);
     input_cloud_sub_ = nh_.subscribe(input_cloud_topic_, 1, &CameraTF::cloudCb, this);
-
-    ifstream ifs(fname);
-
-    string line;
-    vector<string> strvec = split(line, ',');
-
-    getline(ifs, line);
-    strvec = split(line, ',');
-    m11 = std::stof(strvec.at(0));
-    m12 = std::stof(strvec.at(1));
-    m13 = std::stof(strvec.at(2));
-    m14 = std::stof(strvec.at(3));
-
-    getline(ifs, line);
-    strvec = split(line, ',');
-    m21 = std::stof(strvec.at(0));
-    m22 = std::stof(strvec.at(1));
-    m23 = std::stof(strvec.at(2));
-    m24 = std::stof(strvec.at(3));
-
-    getline(ifs, line);
-    strvec = split(line, ',');
-    m31 = std::stof(strvec.at(0));
-    m32 = std::stof(strvec.at(1));
-    m33 = std::stof(strvec.at(2));
-    m34 = std::stof(strvec.at(3));
-
-    std::cout << m11 << " " << m12 << " " << m13 << " " << m14 << std::endl;
-    std::cout << m21 << " " << m22 << " " << m23 << " " << m24 << std::endl;
-    std::cout << m31 << " " << m32 << " " << m33 << " " << m34 << std::endl;
-
-
-
   }
 private:
   ros::NodeHandle nh_;
   ros::Publisher output_cloud_pub_;
   ros::Subscriber input_cloud_sub_;
 
-  double m11, m12, m13, m14;
-  double m21, m22, m23, m24;
-  double m31, m32, m33, m34;
 
   void cloudCb(const sensor_msgs::PointCloud2ConstPtr& msgs)
   {
@@ -87,10 +57,6 @@ private:
       p.y = m21 * x + m22 * y + m23 * z + m24;
       p.z = m31 * x + m32 * y + m33 * z + m34;
 
-      p.x = p.x / 1000;
-      p.y = p.y / 1000;
-      p.z = p.z / 1000;
-
       tf_cloud.push_back(p);
     }
 
@@ -99,28 +65,60 @@ private:
     output_cloud_ros.header.frame_id = robot_frame_;
     output_cloud_pub_.publish(output_cloud_ros);
   }
-
-  vector<string> split(string& input, char delimiter)
-  {
-    istringstream stream(input);
-    string field;
-    vector<string> result;
-    while (getline(stream, field, delimiter)) {
-      result.push_back(field);
-    }
-    return result;
-  }
 };
+
+vector<string> split(string& input, char delimiter)
+{
+  istringstream stream(input);
+  string field;
+  vector<string> result;
+  while (getline(stream, field, delimiter)) {
+    result.push_back(field);
+  }
+  return result;
+}
+
 
 int main(int argc, char** argv)
 {
-  std::cout << "test" << std::endl;
   ros::init(argc, argv, "camera_tf");
 
-  if(argc==2 || argc==3)
-  {
-    std::string fname(argv[1]);
-    CameraTF ct(fname);
-  }
+  std::string fname(argv[1]);
+
+  ifstream ifs(fname);
+
+  std::cout << "file name:" << fname << std::endl;
+
+  string line;
+  vector<string> strvec = split(line, ',');
+
+
+  getline(ifs, line);
+  strvec = split(line, ',');
+  m11 = std::stof(strvec.at(0));
+  m12 = std::stof(strvec.at(1));
+  m13 = std::stof(strvec.at(2));
+  m14 = std::stof(strvec.at(3));
+
+  getline(ifs, line);
+  strvec = split(line, ',');
+  m21 = std::stof(strvec.at(0));
+  m22 = std::stof(strvec.at(1));
+  m23 = std::stof(strvec.at(2));
+  m24 = std::stof(strvec.at(3));
+
+  getline(ifs, line);
+  strvec = split(line, ',');
+  m31 = std::stof(strvec.at(0));
+  m32 = std::stof(strvec.at(1));
+  m33 = std::stof(strvec.at(2));
+  m34 = std::stof(strvec.at(3));
+
+  std::cout << m11 << " " << m12 << " " << m13 << " " << m14 << std::endl;
+  std::cout << m21 << " " << m22 << " " << m23 << " " << m24 << std::endl;
+  std::cout << m31 << " " << m32 << " " << m33 << " " << m34 << std::endl;
+
+  CameraTF ct;
+
   ros::spin();
 }
