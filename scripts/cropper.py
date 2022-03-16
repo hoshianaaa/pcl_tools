62d5fb1b424530b3be4526a3391636af07b3ffb8
hoshianaaa <hoshina54321@gmail.com>
2022-03-16 21:14:35 +0900

    ok crop

--- scripts/cropper.py
+++ scripts/cropper.py
@@ -5,13 +5,47 @@ from __future__ import print_function
 
 import rospy
 from visualization_msgs.msg import *
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import PointCloud2, PointField
 import sensor_msgs.point_cloud2 as pc2
 from geometry_msgs.msg import Point
 import laser_geometry.laser_geometry as lg
 import tf2_ros
 import tf2_geometry_msgs
 import tf2_sensor_msgs.tf2_sensor_msgs
+from std_msgs.msg import Header
+from sensor_msgs import point_cloud2
+
+def list2pc2(list, frame_id):
+
+  points = []
+
+  fields = [PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+            PointField('z', 8, PointField.FLOAT32, 1),
+            # PointField('rgb', 12, PointField.UINT32, 1),
+            PointField('rgba', 12, PointField.UINT32, 1),
+            ]
+
+  for i in range(len(list)):
+
+    x = list[i][0]
+    y = list[i][1]
+    z = list[i][2]
+    r = list[i][3] # 0 ~ 255
+    g = list[i][4] # 0 ~ 255
+    b = list[i][5] # 0 ~ 255
+    a = list[i][6] # 0 ~ 255
+    rgb = struct.unpack('I', struct.pack('BBBB', b, g, r, a))[0]
+
+    pt = [x, y, z, rgb]
+    points.append(pt)
+
+  header = Header()
+  header.frame_id = frame_id
+  pc2 = point_cloud2.create_cloud(header, fields, points)
+  pc2.header.stamp = rospy.Time.now()
+
+  return pc2
 
 X = 0
 Y = 0
@@ -33,6 +67,7 @@ class Raytrace:
         self.listerner = tf2_ros.TransformListener(self.tfBuffer)
         self.lp = lg.LaserProjection()
         self.pub = rospy.Publisher(pub_name, Marker, queue_size=10)
+        self.pub_pc2 = rospy.Publisher("output", PointCloud2, queue_size=10)
         self.sub = rospy.Subscriber(
             sub_name, sub_type, self.pc2_callback)
 
@@ -115,13 +150,22 @@ class Raytrace:
             marker.points.append(p4)
             marker.points.append(p8)
 
-            for p in pc2.read_points(global_cloud, field_names=(
-                    "x", "y", "z"), skip_nans=True):
-                target_point = Point()
-                target_point.x = p[0]
-                target_point.y = p[1]
-                target_point.z = p[2]
+            p_list = []
 
+            for p in pc2.read_points(global_cloud , field_names=(
+                    "x", "y", "z"), skip_nans=True):
+
+              px = p[0]
+              py = p[1]
+              pz = p[2]
+
+              if px >= X - DX/2 and px <= X + DX/2:
+                if py >= Y - DY/2 and py <= Y + DY/2:
+                  if pz >= Z - DZ/2 and pz <= Z + DZ/2:
+                    p_list.append([px,py,pz,255,255,255,255])
+
+            self.pub_pc2.publish(list2pc2(p_list, target_frame))
+            
             self.pub.publish(marker)
             break
 
